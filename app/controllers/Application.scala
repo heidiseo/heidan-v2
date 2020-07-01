e221418aad7b5b9364b4671233caac696b91fe46
heidiseo <heidiseo7@gmail.com>
2020-07-01 21:16:13 +0100

    remove test endpoint

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -19,11 +19,6 @@ object Application extends Controller with DefaultJsonProtocol {
   implicit val activityFormat = Json.format[Activity]
 
   def getAll = Action {
-    val activity = Activity(1, "bike ride", "hyde park", None, "bike ride in hyde park", true)
-    Ok(Json.toJson(activity))
-  }
-
-  def db = Action {
     var id: Int = 1
     var name: String = ""
     var location: String = ""
@@ -35,12 +30,6 @@ object Application extends Controller with DefaultJsonProtocol {
     try {
       val stmt = conn.createStatement
 
-//      val idA = stmt.executeQuery("SELECT id FROM activities")
-//      val nameA = stmt.executeQuery("SELECT name FROM activities")
-//      val locationA = stmt.executeQuery("SELECT location FROM activities")
-//      val costA = stmt.executeQuery("SELECT cost FROM activities")
-//      val descriptionA = stmt.executeQuery("SELECT description FROM activities")
-//      val activities = stmt.executeQuery("SELECT complete FROM activities")
       val activities = stmt.executeQuery("SELECT * FROM activities")
 
       while (activities.next) {
