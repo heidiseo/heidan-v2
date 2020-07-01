6ab3bc698dc8535adbd429253ff17f5b0d68061a
heidiseo <heidiseo7@gmail.com>
2020-07-01 21:29:51 +0100

    add query params for getAll

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,48 +2,48 @@ package controllers
 
 import model.Activity
 
-import scala.collection.mutable.{ArrayBuffer, ListBuffer}
-//import play.api._
-//import play.api.mvc._
-//import play.api.cache.Cache
+import scala.collection.mutable.ListBuffer
 import play.api.Play.current
 import play.api.db._
 import play.api.libs.json.Json
-
 import play.api.mvc._
 import spray.json._
-//import spray.json.DefaultJsonProtocol._
 
 object Application extends Controller with DefaultJsonProtocol {
 
   implicit val activityFormat = Json.format[Activity]
 
-  def getAll = Action {
+  def getAll(completed: Option[Boolean]) = Action {
     var id: Int = 1
     var name: String = ""
     var location: String = ""
     var cost: Option[Double] = Option(1.00)
     var description: String = ""
     var complete: Boolean = true
-    var activity: ListBuffer[Activity] = ListBuffer()
+    var activities: ListBuffer[Activity] = ListBuffer()
     val conn = DB.getConnection()
     try {
       val stmt = conn.createStatement
 
-      val activities = stmt.executeQuery("SELECT * FROM activities")
+      val activitiesFromDB = stmt.executeQuery("SELECT * FROM activities")
 
-      while (activities.next) {
-        id = activities.getInt("id")
-        name = activities.getString("name")
-        location = activities.getString("location")
-        cost = Option(activities.getFloat("cost").toDouble)
-        description = activities.getString("description")
-        complete = activities.getBoolean("complete")
-        activity += Activity(id, name, location, cost, description, complete)
+      while (activitiesFromDB.next) {
+        id = activitiesFromDB.getInt("id")
+        name = activitiesFromDB.getString("name")
+        location = activitiesFromDB.getString("location")
+        cost = Option(activitiesFromDB.getFloat("cost").toDouble)
+        description = activitiesFromDB.getString("description")
+        complete = activitiesFromDB.getBoolean("complete")
+        activities += Activity(id, name, location, cost, description, complete)
       }
     } finally {
       conn.close()
     }
-    Ok(Json.toJson(activity))
+    Ok(
+      completed match {
+        case Some(true) => Json.toJson(activities.filter(_.complete))
+        case Some(false) => Json.toJson(activities.filterNot(_.complete))
+        case _ => Json.toJson(activities)
+      })
   }
 }
